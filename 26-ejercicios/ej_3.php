656607a54c8b8f05c0fef13d648e286cc7d510ee
Lazaro-Belloch <55431104+Lazaro-Belloch@users.noreply.github.com>
2020-12-08 15:58:05 +0100

    fin PHP plano

---
+++ 26-ejercicios/ej_3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Ejercicios</title>
+</head>
+
+<body class=container>
+    <h1>Ejercicio 3</h1>
+<hr>
+
+    <?php
+    /*
+     * Hacer una interfaz de usuario(formulario) con dos inputs
+     * y cuatro botones, uno para sumar, restar, multiplicar y dividir
+     */
+
+    
+
+
+?>
+
+
+    
+</body>
+
+</html>
